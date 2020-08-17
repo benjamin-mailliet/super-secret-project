a8afe5ed6459c040804f865f76534f84fc48e3c2
Benjamin Mailliet <benjamin.mailliet@santeos.com>
2020-08-17 18:04:23 +0200

    FEAT(Bullet time): Ajout du bullet time sur shift gauche

---
+++ Assets/Scripts/TimeFlowManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeFlowManager : MonoBehaviour
+{
+
+    public float slowdownFactor;
+    private float fixedDeltaTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Awake() {
+        // Make a copy of the fixedDeltaTime, it defaults to 0.02f, but it can be changed in the editor
+        this.fixedDeltaTime = Time.fixedDeltaTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire3")) {
+            Debug.Log("Bullet time activated");
+            DoSlowMotion();
+        }
+        if (Input.GetButtonUp("Fire3")) {
+            Debug.Log("Bullet time deactivated");
+            StopSlowMotion();
+        }
+        Time.fixedDeltaTime = this.fixedDeltaTime * Time.timeScale;
+    }
+
+    private void DoSlowMotion() {
+        if(Time.timeScale == 1.0f) {
+            Time.timeScale = slowdownFactor;
+        }        
+    }
+
+    private void StopSlowMotion() {
+        if (Time.timeScale != 1.0f) {
+            Time.timeScale = 1.0f;
+        }
+    }
+}
